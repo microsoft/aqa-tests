be2c002b4127ca11cf5d9dbb0ecb0761358caa49
Stephanie Crater <scrater@microsoft.com>
2023-01-24 08:42:18 -0800

    Add initial ycsb support

---
+++ perf/ycsb/getPropertiesFiles.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+true=0
+false=1
+
+propFilesExist() {
+    propFiles=(`find . -type f -name "*.properties"`)
+    if [ ${#propFiles[@]} -gt 0 ]; then 
+        echo "Found ${#propFiles[@]} properties files:"
+        echo ${propFiles[*]}
+        return $true
+    else 
+        echo "No properties files found."
+        return $false
+    fi 
+}
+
+ycsb_dir=$1
+if propFilesExist; then
+    echo "Copying properties files to $ycsb_dir"
+    cp *.properties $ycsb_dir
+fi
